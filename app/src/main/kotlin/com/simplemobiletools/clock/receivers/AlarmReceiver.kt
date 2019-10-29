3d6bef00a8116114984ebbbc0113d7b4404a4550
coxtor <contact@vcranz.de>
2019-10-29 18:41:06 +0100

    merge conflicts

--- app/src/main/kotlin/com/simplemobiletools/clock/receivers/AlarmReceiver.kt
+++ app/src/main/kotlin/com/simplemobiletools/clock/receivers/AlarmReceiver.kt
@@ -11,18 +11,37 @@ import com.simplemobiletools.clock.helpers.ALARM_ID
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val id = intent.getIntExtra(ALARM_ID, -1)
-        val alarm = context.dbHelper.getAlarmWithId(id) ?: return
-
-        if (context.isScreenOn()) {
-            context.showAlarmNotification(alarm)
-            Handler().postDelayed({
-                context.hideNotification(id)
-            }, context.config.alarmMaxReminderSecs * 1000L)
-        } else {
-            Intent(context, ReminderActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                putExtra(ALARM_ID, id)
-                context.startActivity(this)
+        val alarm = context.dbHelper.getAlarmWithId(id)
+        if(alarm!!.isChild) {
+            Intent().also { intent ->
+                intent.setAction("com.simplemobiletools.ALARM_GOING_TO_RING")
+                intent.putExtra("minutes", alarm?.timeInMinutes)
+                intent.putExtra("days", alarm?.days)
+                intent.putExtra("id", alarm?.id)
+                intent.putExtra("custom_payload", alarm?.label)
+                context.sendBroadcast(intent)
+            }
+        }
+        else {
+            val alarm = context.dbHelper.getAlarmWithId(id) ?: return
+            if (context.isScreenOn()) {
+                context.showAlarmNotification(alarm)
+                Handler().postDelayed({
+                    context.hideNotification(id)
+                }, context.config.alarmMaxReminderSecs * 1000L)
+            } else {
+                Intent(context, ReminderActivity::class.java).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    putExtra(ALARM_ID, id)
+                    context.startActivity(this)
+                }
+            }
+            Intent().also { intent ->
+                intent.setAction("com.simplemobiletools.ALARM_IS_RINGING")
+                intent.putExtra("minutes", alarm.timeInMinutes)
+                intent.putExtra("days", alarm.days)
+                intent.putExtra("id", alarm.id)
+                context.sendBroadcast(intent)
             }
         }
     }
